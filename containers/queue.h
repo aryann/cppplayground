33175227928a4e3c9a7149716dc099442f2148c0
Aryan Naraghi <aryan.naraghi@gmail.com>
2025-08-16 11:32:49 -0700

    Adds top() and aliases

--- containers/queue.h
+++ containers/queue.h
@@ -5,10 +5,22 @@
 
 namespace containers {
 
-template <typename T, typename Container = std::vector<T>>
-class priority_queue {
+template <class T, class Container = std::vector<T>> class priority_queue {
 public:
-  [[nodiscard]] bool empty() { return true; }
+  using container_type = Container;
+  using value_type = typename Container::value_type;
+  using size_type = typename Container::size_type;
+  using reference = typename Container::reference;
+  using const_reference = typename Container::const_reference;
+
+  [[nodiscard]] auto top() const -> const_reference {
+    return container_.front();
+  }
+
+  [[nodiscard]] auto empty() const -> bool { return container_.empty(); }
+
+private:
+  Container container_;
 };
 
 } // namespace containers
