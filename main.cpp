7c7d3f535fefa319fd859eb831397c6f74611cf2
Aryan Naraghi <aryan.naraghi@gmail.com>
2025-08-16 10:22:13 -0700

    Adds more configs

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
 #include <iostream>
 
-int main(int, char **) { std::cout << "Hello, from cppplayground!\n"; }
+int main(int, char **) {
+  int x = 1;
+  int y = 1;
+
+  std::cout << "Hello, from cppplayground!\n";
+  return y - x;
+}
