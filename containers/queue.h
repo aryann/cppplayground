b2c43ea95a593a06fe10b0b7c95fcf03f1defee0
Aryan Naraghi <aryan.naraghi@gmail.com>
2025-08-16 12:27:40 -0700

    Initial working implementation of priority_queue

--- containers/queue.h
+++ containers/queue.h
@@ -1,6 +1,7 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
+#include <optional>
 #include <vector>
 
 namespace containers {
@@ -17,15 +18,79 @@ public:
     return container_.front();
   }
 
-  auto push(const value_type &val) -> void {}
-  auto push(value_type &&val) -> void {}
+  auto push(const value_type &val) -> void {
+    container_.push_back(val);
+    percolate_up();
+  }
+  auto push(value_type &&val) -> void {
+    container_.push_back(std::move(val));
+    percolate_up();
+  }
 
-  auto pop() -> void {}
+  auto pop() -> void {
+    std::swap(container_.front(), container_.back());
+    container_.pop_back();
+    percolate_down();
+  }
 
   [[nodiscard]] auto empty() const -> bool { return container_.empty(); }
   [[nodiscard]] auto size() const -> size_type { return container_.size(); }
 
 private:
+  void percolate_up() {
+    int curr = container_.size() - 1;
+    while (true) {
+      std::optional<int> parent = find_parent(curr);
+      if (!parent) {
+        break;
+      }
+
+      if (container_[*parent] <= container_[curr]) {
+        break;
+      }
+
+      std::swap(container_[curr], container_[*parent]);
+      curr = *parent;
+    }
+  }
+
+  void percolate_down() {
+    int curr = 0;
+    while (curr + 1 < container_.size()) {
+      auto [child_idx, child_val] = min_child(curr);
+      if (container_[curr] <= child_val) {
+        break;
+      }
+
+      std::swap(container_[curr], container_[child_idx]);
+      curr = child_idx;
+    }
+  }
+
+  [[nodiscard]] static std::optional<int> find_parent(int i) {
+    if (i < 1) {
+      return std::nullopt;
+    }
+
+    return (i - 1) / 2;
+  }
+
+  [[nodiscard]] std::pair<int, int> min_child(int i) {
+    std::pair<int, int> result;
+
+    int left = i * 2 + 1;
+    if (left < container_.size()) {
+      result = {left, container_[left]};
+    }
+
+    int right = i * 2 + 2;
+    if (right < container_.size() && container_[right] < container_[left]) {
+      return {right, container_[right]};
+    }
+
+    return result;
+  }
+
   Container container_;
 };
 
--- containers/queue_test.cc
+++ containers/queue_test.cc
@@ -4,11 +4,46 @@
 
 namespace containers {
 
+using ::testing::ElementsAre;
 using ::testing::IsTrue;
 
 TEST(PriorityQueue, Empty) {
   priority_queue<int> q;
   EXPECT_THAT(q.empty(), IsTrue());
+}
+
+TEST(PriorityQueue, PushNonDecreasing) {
+  priority_queue<int> q;
+  q.push(1);
+  q.push(2);
+  q.push(3);
+  q.push(4);
+  q.push(5);
+
+  std::vector<int> vals;
+  while (!q.empty()) {
+    vals.push_back(q.top());
+    q.pop();
+  }
+
+  EXPECT_THAT(vals, ElementsAre(1, 2, 3, 4, 5));
+}
+
+TEST(PriorityQueue, PushNonIncreasing) {
+  priority_queue<int> q;
+  q.push(5);
+  q.push(4);
+  q.push(3);
+  q.push(2);
+  q.push(1);
+
+  std::vector<int> vals;
+  while (!q.empty()) {
+    vals.push_back(q.top());
+    q.pop();
+  }
+
+  EXPECT_THAT(vals, ElementsAre(5, 4, 3, 2, 1));
 }
 
 } // namespace containers
